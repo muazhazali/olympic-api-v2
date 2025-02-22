deef58ab678dd41022aada5cae2613e733d037e9
Muaz Hazali <100086292+muazhazali@users.noreply.github.com>
2025-02-22 23:04:30 +0800

    update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,3 +1,27 @@
+-- Table: public.temp_olympic_athlete_biography
+
+-- DROP TABLE IF EXISTS public.temp_olympic_athlete_biography;
+
+CREATE TABLE IF NOT EXISTS public.temp_olympic_athlete_biography
+(
+    athlete_id text COLLATE pg_catalog."default",
+    name text COLLATE pg_catalog."default",
+    sex text COLLATE pg_catalog."default",
+    born text COLLATE pg_catalog."default",
+    height text COLLATE pg_catalog."default",
+    weight text COLLATE pg_catalog."default",
+    country text COLLATE pg_catalog."default",
+    country_noc text COLLATE pg_catalog."default",
+    description text COLLATE pg_catalog."default",
+    special_notes text COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.temp_olympic_athlete_biography
+OWNER to postgres;
+
+-- insert data into temp_olympic_athlete_biography
 INSERT INTO public.olympic_athlete_biography (athlete_id, name, sex, born, height, weight, country, country_noc, description, special_notes)
 SELECT 
     athlete_id, 
